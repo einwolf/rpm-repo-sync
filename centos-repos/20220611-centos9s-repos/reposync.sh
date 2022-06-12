3a34a45b3703a4be61912300dc480c4aaf5c5ba8
Cole Wyman <36602061+einwolf@users.noreply.github.com>
2022-06-12 07:50:16 -0400

    Centos 9s missing crb

--- centos-repos/20220611-centos9s-repos/reposync.sh
+++ centos-repos/20220611-centos9s-repos/reposync.sh
@@ -2,7 +2,7 @@
 
 dnf reposync --repo appstream -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
 dnf reposync --repo baseos -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
-#dnf reposync --repo extras -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
+dnf reposync --repo crb -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
 dnf reposync --repo extras-common -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
 dnf reposync --repo epel -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
 dnf reposync --repo epel-next -a x86_64,noarch --newest-only --delete --downloadcomps --download-metadata
